9c0295ba1535898a3fc3c5227288bb2614abd048
tsctsc6 <tsctsc6@126.com>
2025-09-28 16:16:24 +0800

    完成对ip改变事件的监听

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use futures::StreamExt;
+use if_watch::smol::IfWatcher;
+use if_watch::{IfEvent, IpNet};
+
+#[tokio::main]
+async fn main() {
+    let mut set = IfWatcher::new().unwrap();
+    loop {
+        let event = set.select_next_some().await;
+        let event = match event {
+            Ok(r) => r,
+            Err(_) => continue,
+        };
+        let ip = match event {
+            IfEvent::Up(ip) => ip,
+            IfEvent::Down(_) => continue,
+        };
+        let ip = match ip {
+            IpNet::V4(_) => continue,
+            IpNet::V6(ipv6) => ipv6,
+        };
+        if ip.addr().is_loopback() {
+            continue;
+        }
+        if ip.addr().is_unique_local() {
+            continue;
+        }
+        println!("{}", ip);
+    }
 }

ac234434b14b67f4d95ba4db91acd33002dbd4d0
tsctsc6 <tsctsc6@126.com>
2025-10-11 19:27:16 +0800

    update script

--- scripts/cloudflared.ps1
+++ scripts/cloudflared.ps1
@@ -2,6 +2,8 @@ $ZONE_ID = "xx"
 $DNS_RECORD_ID = "xx"
 $uri = "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$DNS_RECORD_ID"
 
+$ip = (Get-NetIPAddress -AddressFamily IPv6 -InterfaceAlias "以太网" -PrefixOrigin RouterAdvertisement -SuffixOrigin Random).IPAddress
+
 $token = ConvertTo-SecureString "xx" -AsPlainText -Force
 
 $params = @{
@@ -10,18 +12,18 @@ $params = @{
     ContentType    = 'application/json'
     Authentication = 'Bearer'
     Token          = $token
-    Body           = '{
-        "name":"xx",
-        "type":"AAAA",
-        "content":"xx",
-        "proxiable":true,
-        "proxied":false,
-        "ttl":1,
-        "settings":{},
-        "meta":{},
-        "comment":null,
-        "tags":[]
-    }'
+    Body           = "{
+        `"name`":`"xx`",
+        `"type`":`"AAAA`",
+        `"content`":`"$ip`",
+        `"proxiable`":true,
+        `"proxied`":false,
+        `"ttl`":1,
+        `"settings`":{},
+        `"meta`":{},
+        `"comment`":null,
+        `"tags`":[]
+    }"
 }
 
 $response = Invoke-WebRequest @params

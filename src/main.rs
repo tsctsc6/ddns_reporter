4df242e60c1c7aff74b5fa9f40da65de8d6d7677
tsctsc6 <tsctsc6@126.com>
2025-12-03 12:16:01 +0800

    监听特定的网络接口

--- src/main.rs
+++ src/main.rs
@@ -150,13 +150,57 @@ async fn main() {
     let rt_handle = Handle::current().clone();
 
     let register_handler_result = netwatcher::watch_interfaces(move |update| {
+        let network_name = network_name.clone();
+        let network_index = update
+            .interfaces
+            .iter()
+            .filter(|x| -> bool { x.1.name == network_name })
+            .map(|x| -> u32 { *x.0 })
+            .next();
+        let network_index = match network_index {
+            None => {
+                error!("Interface {} not found", network_name);
+                return;
+            }
+            Some(i) => {
+                info!("Interface {} found: {}", network_name, i);
+                i
+            }
+        };
+
         debug!("Interfaces added: {:?}", update.diff.added);
         debug!("Interfaces removed: {:?}", update.diff.removed);
-        for (ifindex, if_diff) in update.diff.modified {
-            debug!("Interface index {} has changed", ifindex);
-            debug!("Added IPs: {:?}", if_diff.addrs_added);
-            debug!("Removed IPs: {:?}", if_diff.addrs_removed);
+
+        if update.diff.added.contains(&network_index) {
+            // Check if we're currently in a Tokio runtime context
+            if Handle::try_current().is_ok() {
+                // If yes, use block_in_place to temporarily yield the async context
+                block_in_place(|| rt_handle.block_on(debouncer_arc.trigger()))
+            } else {
+                // If not, directly block_on with the cloned handle
+                rt_handle.block_on(debouncer_arc.trigger());
+            }
+            return;
         }
+
+        debug!("{:?}", update.diff.modified);
+
+        let network_diff = update
+            .diff
+            .modified
+            .iter()
+            .filter(|x| -> bool { *x.0 == network_index })
+            .next();
+        let network_diff = match network_diff {
+            None => {
+                debug!("Interface {} not found in network_diff", network_name);
+                return;
+            }
+            Some(d) => d,
+        };
+        debug!("Interface index {} has changed", network_diff.0);
+        debug!("Added IPs: {:?}", network_diff.1.addrs_added);
+        debug!("Removed IPs: {:?}", network_diff.1.addrs_removed);
         // Check if we're currently in a Tokio runtime context
         if Handle::try_current().is_ok() {
             // If yes, use block_in_place to temporarily yield the async context
